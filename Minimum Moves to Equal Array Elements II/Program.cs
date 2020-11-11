617d4f5b7b3c1f9667ecd0a4e2efc719e5dd43e6
Jaydeep Shil <ED8A@eurofins.com>
2020-11-11 20:55:46 +0530

    Add project files.

---
+++ Minimum Moves to Equal Array Elements II/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Minimum_Moves_to_Equal_Array_Elements_II
+{
+    // https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[] { 1, 2, 3 };
+            Console.WriteLine(MinMoves2(nums));
+        }
+
+        static int MinMoves2(int[] nums)
+        {
+            Array.Sort(nums);
+            int i = 0, j = nums.Length - 1;
+            int minMove = 0;
+            while (i < j)
+            {
+                minMove += nums[j--] - nums[i++];
+            }
+            return minMove;
+        }
+    }
+}
